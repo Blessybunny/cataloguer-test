f1bc3e8ba111a223ae29a63e4631fc664ef2162e
Blessybunny <45063174+Blessybunny@users.noreply.github.com>
2023-11-15 22:04:57 +0800

    Misc

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -8,6 +8,12 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
+#echo "Running migrations..."
+#php artisan migrate --force 
+
 echo "Running migrations..."
-php artisan migrate --force 
+php artisan migrate:fresh
+
+echo "Running seeders..."
+php artisan db:seed
 
