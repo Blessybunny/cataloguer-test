150962dd6f0f947808b40d00f91aa53a11fa3655
Blessybunny <45063174+Blessybunny@users.noreply.github.com>
2023-11-25 22:07:36 +0800

    Misc

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator; // https://render.com/docs/deploy-php-laravel-docker
-use Illuminate\Support\Facades\Schema; // https://www.quora.com/Why-does-the-PHP-artisan-migrate-command-not-create-new-tables-in-my-database-except-by-default-tables-users-migrations-and-password-resets
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
