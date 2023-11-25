ff99759aa35fab1c483c97db4d82012160414fdf
Blessybunny <45063174+Blessybunny@users.noreply.github.com>
2023-11-26 01:11:39 +0800

    Test

--- database/migrations/2023_11_25_170122_create_sample_table.php
+++ database/migrations/2023_11_25_170122_create_sample_table.php
@@ -4,17 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('sample', function (Blueprint $table) {
+return new class extends Migration {
+    public function up () : void {
+        Schema::create('students', function (Blueprint $table) {
             // ALL -> Learner's information
             $table->id(); // warning: LRN is impossibly long for an integer
-            $table->timestamps();
             $table->string('li_name_last', 100);
             $table->string('li_name_first', 100);
             $table->string('li_name_middle', 100);
@@ -668,12 +662,8 @@ return new class extends Migration
             $table->string('SF9_g10_values_mb_r2_qr4', 5)->nullable();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('sample');
+    
+    public function down () : void {
+        Schema::dropIfExists('students');
     }
 };
